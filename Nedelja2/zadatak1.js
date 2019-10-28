c4b1a34ddb48d08395def442ae00f5f0781d8466
slobodan <slobodanjosipovic@gmail.com>
2019-10-28 18:59:51 +0100

    domaci od ponedeljka

---
+++ Nedelja2/zadatak1.js
@@ -0,0 +1,15 @@
+let a =10;
+let b =5;
+ let c=a+b;
+ console.log(c);
+
+ let d=a-b;
+ console.log(d);
+ let e=a*b;
+ console.log(e)
+ let f=a/b;
+ console.log(f);
+ let g=a%b;
+ console.log(g);
+ let h=Math.pow(a,b);
+ console.log(h);
---
+++ Nedelja2/zadatak2/zadatak2.js
@@ -0,0 +1,13 @@
+let dom='Welcome to DOM';
+
+
+let a=document.getElementById('heading');
+a.innerHTML=dom;
+
+let b=document.getElementById('added');
+b.innerHTML=`<p>dodati tekst</p>`;
+
+
+
+
+
