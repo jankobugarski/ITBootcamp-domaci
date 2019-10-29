8baf3c6f56515dc0d66e155418767a524e393519
slobodan <slobodanjosipovic@gmail.com>
2019-10-29 22:06:57 +0100

    domaci za sredu

---
+++ Nedelja2/zadatak3.js
@@ -0,0 +1,9 @@
+let broj =21;
+
+if(broj % 3===0){
+
+    console.log('broj je deljiv sa 3');
+}
+else{
+    console.log('broj nije deljiv sa 3');
+}
---
+++ Nedelja2/zadatak4.js
@@ -0,0 +1,20 @@
+// 1.posto nisam bas siguran sta se trazi,prvo cu napisati sve brojeve iz prvih 1000 koji su deljivi sa 5,a zatim prvih 1000brojeva deljiv sa 
+
+for(i=0; i<=1000;i++){
+
+if(i % 5===0){
+
+    console.log(i);
+}
+
+}
+
+
+for(j=0; j<=5000;j++){
+
+
+    if(j % 5 ===0){
+
+        console.log(j);
+    }
+}
