ba5dde17d0cb49bc96ec35f1dcae5b53c8804483
slobodan <slobodanjosipovic@gmail.com>
2019-11-05 15:49:55 +0100

    poruka

--- Nedelja3/zadatak1.js
+++ Nedelja3/zadatak1.js
@@ -19,3 +19,22 @@ else { return c;
 
 
 console.log(maxNumber(2,3,12));
+
+
+
+function pizzaPrice(p, r) {
+
+
+
+
+
+    let povrsina = Math.pow(r, 2) * Math.PI;
+
+let price=p/povrsina;
+
+return price;
+
+}
+
+
+console.log(pizzaPrice(2000,16))
