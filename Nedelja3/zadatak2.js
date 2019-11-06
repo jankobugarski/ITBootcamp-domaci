a05cd0b5f944f26b95e5a6837dd870f988e568ef
slobodan <slobodanjosipovic@gmail.com>
2019-11-06 12:05:13 +0100

    nedelja1

--- Nedelja3/zadatak2.js
+++ Nedelja3/zadatak2.js
@@ -34,3 +34,27 @@ else { return c;
 }
 }
 console.log(`najjeftinija pizza je ${maxNumber(pizza1,pizza2,pizza3) }`)    
+
+//  jos jedna ideja za zadatak
+
+
+let d;
+function cenaPizze(r1,r2,r3,p1,p2,p3,fn){
+
+
+    let povrsina1 = Math.pow(r1, 2) * Math.PI;
+    let povrsina2 = Math.pow(r2, 2) * Math.PI;
+    let povrsina3 = Math.pow(r3, 2) * Math.PI;
+
+    let price1=p1/povrsina1;
+    let price2=p2/povrsina2;
+    let price3=p3/povrsina3;
+
+    d = fn(price1,price2,price3);
+
+
+
+    
+}
+cenaPizze(25,16,18,1000,1900,1500,maxNumber);
+console.log(d)
