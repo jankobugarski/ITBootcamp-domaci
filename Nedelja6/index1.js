46209f2b73f2f5745b6870b1b198bbef84357107
slobodan <slobodanjosipovic@gmail.com>
2019-11-30 23:49:22 +0100

    domaci

---
+++ Nedelja6/index1.js
@@ -0,0 +1,25 @@
+// 1.   Json koristimo za komunikaciju izmedju klijenta 
+//      i servera.Preko Json-a primamo neke podatke od servera i saljemo
+//      podatke serveru
+
+// 2.  Moduli su deo js skript koda i sluze da se da se jedan projekat podeli na vise medjusobno
+//     povezanih delova,sto pogotovu ima smisla kod velikih projekata.Postoji dosta prednosti pri koriscenju modula.Neke od prednosti su to 
+//     sto ne zavise od ostalog koda i mogu nezavisno od njega da se menjaju i dalje razvijaju.
+//     Dalje,omogucava nam da delimo samo varijable koje hocemo,sa drugim delovima koda.Takodje,
+//     olaksavaju nam kopiranje pojedinih delova koda u nekim drugim projektima,to jest,lako se 
+//     nadje kod i stedi se vreme.
+       
+    // 3.Ecmascript6 je novija,unapredjena verzija javascripta koja ispravlja neke nedostatke
+    //  javascripta i omogucava njegovu vecu primenu
+    
+    // 4. HTTP zahtev formira browser kad zelimo da trazimo nesto od servera .Taj zahtev se prenosi 
+    //  preko protokola i prima ga api servera i prosledjuje do servera.On se sastoji od headera,
+    //  metode(get,put,post,delete..)i body-ja.Api je deo servera,to je zapravo interfejs koji 
+        // omogucava komunikaciju izmedju klijenta i ServiceUIFrameContext.On prima HTTp zahtev i
+        // i prosledjuje odgovor 
+        
+        // 5.Dvostruko jednako pokazuje da je nesto jednako po vrednosti,a trostruko da je 
+        // jednako po tipu i po vrednosti.
+
+        // 6.Odgovor je String,zato sto je typeof true boolian,i taj odgovor kad se ispise 
+        // u consoli je string(kao i ispis bilo kog tipa podataka).Typeof ispisa je string,naravno.
---
+++ Nedelja6/index2.js
@@ -0,0 +1,41 @@
+let posalji = document.getElementById('dugme');
+let niz = []
+let objekat = {}
+
+posalji.addEventListener('click', () => {
+    let text = document.getElementById('username')
+    let lozinka = document.getElementById('password')
+    text = text.value;
+    lozinka = lozinka.value
+    let a=lozinka.length;
+    let b=text.length
+
+    if(a>=3 &&a<=15 && b>=3 && b<=15){
+        objekat.username = text;
+        objekat.password = lozinka;
+    }
+    else{
+        alert('lozinka i korisnicko ime moraju da sadrzi izmedju 3 i 15 karaktera i lozinka ne sme da pocne brojem')
+    }
+    niz.push(objekat)
+    console.log(niz)
+    let json=JSON.stringify(niz)
+    console.log(json)
+    
+
+    
+   
+    
+
+})
+
+    
+    
+
+   
+    
+
+    
+
+
+
